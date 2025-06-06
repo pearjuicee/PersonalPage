21da30aa36f4cbc2d60d410c7b70b18b28bf736c
Shirley Ding <122686936+pearjuicee@users.noreply.github.com>
2025-06-05 22:37:49 -0400

    fix image paths

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -7,7 +7,7 @@ const projects = [
     description: "A virtual dressing room to simplify virtual try-ons and make it easier to see how clothes fit before you buy. People's Choice Award at McHacks 12.",
     tags: ["Python", "Javascript", "Flask", "Auth0", "MediaPipe", "OpenCV", "HTML", "CSS"],
     link: "https://devpost.com/software/looksy-a-virtual-dressing-room",
-    image: "/assets/looksy.png",
+    image: "/shirley-portfolio/assets/looksy.png",
     date: "01/2025"
   },
   {
@@ -15,7 +15,7 @@ const projects = [
     description: "Nine Lives Later is a completed visual novel by a group of cat lovers and beginner game developers with 100+ browser plays on itch.io. Winner of Best Art.",
     tags: ["Unity", "C#", "Visual Novel", "Game Development"],
     link: "https://starberrykiruya.itch.io/ninelives",
-    image: "/assets/nll.png",
+    image: "/shirley-portfolio/assets/nll.png",
     date: "01/2025"
   },
   {
@@ -23,7 +23,7 @@ const projects = [
     description: "Dictation tool made for Girls Who Code Hackathon. An online teacher that help students to practice their dictation and get that A. First place winner at Girls Who Hack 2024.",
     tags: ["Python", "Flask", "HTML", "CSS", "Javascript", "Web Speech API"],
     link: "https://devpost.com/software/dictaction",
-    image: "/assets/dictaction.png",
+    image: "/shirley-portfolio/assets/dictaction.png",
     date: "03/2024"
   },
   {
@@ -31,7 +31,7 @@ const projects = [
     description: " Implemented logistic regression to predict heart disease likelihood based on health indicators like cholesterol, age, and blood pressure, using scikit-learn.",
     tags: ["Python", "Flask", "Pandas", "NumPy", "Scikit-learn", "Matplotlib", "Figma"],
     link: "#",
-    image: "/assets/heartmap1.png",
+    image: "/shirley-portfolio/assets/heartmap1.png",
     date: "11/2024 - 12/2024"
   }
 ]
